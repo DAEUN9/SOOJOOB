584f605c8aa89207282aae0790f4e86b40a87314
daeun <asd5687@naver.com>
2022-08-01 17:51:06 +0900

    #S07P12D210-228 플로깅 생성api까지 완료

---
+++ BackEnd/daeun-clear/src/main/java/com/D210/soojoobback/dto/plogging/PostPloggingReqDto.java
@@ -0,0 +1,22 @@
+package com.D210.soojoobback.dto.plogging;
+
+import com.D210.soojoobback.entity.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class PostPloggingReqDto {
+
+    private Long userId;
+    private Double distance;
+    private String startTime;
+    private String endTime;
+    private Integer stepCount;
+    private Integer trashCount;
+    private String floggingImg;
+    private Double calorie;
+    private Double bpm;
+
+
+}
