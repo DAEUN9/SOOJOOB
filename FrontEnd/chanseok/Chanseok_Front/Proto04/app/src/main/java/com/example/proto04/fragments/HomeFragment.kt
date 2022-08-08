e2cc3f420afc3cd743ee4cf2314ee460d25889e1
SSAFY <ict.cspark@gmail.com>
2022-08-08 14:34:07 +0900

    proto04 upload

---
+++ FrontEnd/chanseok/Chanseok_Front/Proto04/app/src/main/java/com/example/proto04/fragments/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.example.proto04.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.proto04.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var mBinding : FragmentHomeBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        mBinding = binding
+
+        return mBinding?.root
+    }
+
+    override fun onDestroyView() {
+        mBinding = null
+        super.onDestroyView()
+    }
+
+
+}
