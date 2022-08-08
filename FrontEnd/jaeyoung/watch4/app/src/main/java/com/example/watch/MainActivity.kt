f0acc0aaad6366e174b8977e1b533acbd6c1bf15
SSAFY <wodud5381@naver.com>
2022-08-08 14:44:45 +0900

    watch proto type

---
+++ FrontEnd/jaeyoung/watch4/app/src/main/java/com/example/watch/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.watch
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.watch.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
