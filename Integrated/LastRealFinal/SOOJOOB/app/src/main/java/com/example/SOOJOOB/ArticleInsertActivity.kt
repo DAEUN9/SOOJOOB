0e58a20aad78f20f65ec275c85fe4407223060d9
SSAFY <ict.cspark@gmail.com>
2022-08-19 02:00:05 +0900

    last real final

---
+++ Integrated/LastRealFinal/SOOJOOB/app/src/main/java/com/example/SOOJOOB/ArticleInsertActivity.kt
@@ -0,0 +1,123 @@
+package com.example.SOOJOOB
+
+import android.Manifest
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.SOOJOOB.databinding.ActivityArticleInsertBinding
+import com.example.SOOJOOB.fragments.CommunityFragment
+import com.example.SOOJOOB.retrofit.ArticleWork
+import java.io.ByteArrayOutputStream
+import java.lang.Exception
+
+class ArticleInsertActivity  : AppCompatActivity(){
+    private lateinit var imageView: ImageView
+    private lateinit var uploadImageBtn: ImageView
+    val Gallery = 0
+    private lateinit var bitmap:Bitmap
+
+
+
+    private val binding by lazy {
+        ActivityArticleInsertBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var title: TextView
+    private lateinit var contents: TextView
+
+    private val permissionList = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+    private val checkPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
+        result.forEach {
+            if(!it.value) {
+                Toast.makeText(applicationContext, "권한 동의 필요!", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_park)
+
+        binding.btnWrite.setOnClickListener{
+
+            val articleData = ArticleRequestBody(
+                binding.title.text.toString(),
+                binding.contents.text.toString(),
+                encodeImage(bitmap),
+
+                )
+            val insertWork = ArticleWork(articleData)
+            val intent = Intent(this, CommunityFragment::class.java)
+            insertWork.work(completion = { status, msg ->
+                if (status in 200..300){
+                    Toast.makeText(this@ArticleInsertActivity, "$msg", Toast.LENGTH_SHORT).show()
+                    startActivity(intent)
+                }else{
+                    val msg = msg?.substring(25 until 37)
+                    Toast.makeText(this@ArticleInsertActivity, "$msg", Toast.LENGTH_SHORT).show()
+
+                }
+            } )
+        }
+        binding.backCommunity.setOnClickListener {
+            super.onBackPressed()
+        }
+
+        binding.btnCancel.setOnClickListener{
+            super.onBackPressed()
+        }
+
+        binding.uploadImageBtn.setOnClickListener {
+            loadImage()
+        }
+
+
+        checkPermission.launch(permissionList)
+
+    }
+
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+
+        if(resultCode == RESULT_OK){
+            val dataUri = data?.data
+            try{
+                bitmap= MediaStore.Images.Media.getBitmap(this.contentResolver,dataUri)
+                binding.uploadImageBtn.setImageBitmap(bitmap)
+                println("성공")
+            }catch (e: Exception){
+                Toast.makeText(this,"$e", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun loadImage(){
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent,"load picture"),Gallery)
+    }
+    private fun encodeImage(bm: Bitmap): String {
+        val baos = ByteArrayOutputStream()
+        bm.compress(Bitmap.CompressFormat.PNG, 100, baos)
+        val b = baos.toByteArray()
+        return Base64.encodeToString(b, Base64.DEFAULT)
+    }
+
+
+}
---
+++ Integrated/LastRealFinal/SOOJOOB/app/src/main/java/com/example/SOOJOOB/fragments/RecordFragment.kt
@@ -0,0 +1,156 @@
+package com.example.SOOJOOB.fragments
+
+
+import android.app.Activity
+import android.content.Intent
+import com.example.SOOJOOB.databinding.FragmentRecordBinding
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.SOOJOOB.*
+import com.example.SOOJOOB.RecyclerAdapter
+import com.example.SOOJOOB.retrofit.PloggingGetResponseBody
+import com.example.SOOJOOB.retrofit.PloggingResult
+import com.example.SOOJOOB.retrofit.RetrofitAPI
+import retrofit2.Call
+import retrofit2.Response
+
+class RecordFragment : Fragment() {
+
+    private lateinit var fBinding : FragmentRecordBinding
+    private lateinit var trashSortBtn:Button
+    private lateinit var dateSortBtn:Button
+    private lateinit var nextMapsBtn:ImageView
+    private lateinit var rankingButton:ImageView
+
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//
+//        val binding = FragmentRecordBinding.inflate(inflater, container, false)
+//
+//        fBinding = binding
+//        recyler_view = binding.recylerView
+//        println(binding.recylerView)
+//
+//
+//        return fBinding?.root
+//    }
+//
+//    override fun onDestroyView() {
+////        fBinding = null
+//        super.onDestroyView()
+//    }
+
+    fun work(){
+        val service = RetrofitAPI.ploggingService
+
+        // gson.toJson 을 이용해서 Json 으로 변경
+
+
+//        Call 작업은 두 가지로 실행됨
+//        execute 를 사용하면 request 를 보내고 response 를 받는 행위를 동기적으로 수행한다.
+//        enqueue 작업을 실행하면 request 는 비동기적으로 보내고, response 는 콜백으로 받게 된다.
+
+        service.getPlogging()
+            .enqueue(object : retrofit2.Callback<PloggingGetResponseBody> {
+                override fun onResponse(
+                    call: Call<PloggingGetResponseBody>,
+                    response: Response<PloggingGetResponseBody>
+                ) {
+                    if (response.isSuccessful) {
+                        val result = response.body()?.result?.reversed()
+                        result?.let {
+                            it?.let { it1 -> setAdapter(it1) }
+                        }
+
+                        val dateSort = response.body()?.result
+                        trashSortBtn.setOnClickListener {
+                            val trashSort = response.body()?.result?.sortedWith(compareBy<PloggingResult>{it.trashCount})?.reversed()
+                            trashSort?.let {
+                                it.let{it1 -> setAdapter(it1)}
+                            }
+                        }
+                        dateSortBtn.setOnClickListener {
+                            val dateSort = response.body()?.result?.reversed()
+                            dateSort?.let {
+                                it.let{it1 -> setAdapter(it1)}
+                            }
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<PloggingGetResponseBody>, t: Throwable) {
+                    Log.d("플로깅  get 실패", t.message.toString())
+                }
+            })
+    }
+
+//
+//    private fun setAdapter(PloggingList : List<Result>){
+//        val mAdapter = RecyclerAdapter(PloggingList)
+//        recyler_view.adapter = mAdapter
+////        recyler_view.layoutManager = LayoutManager(this,2, GridLayoutManager.VERTICAL,
+////            false)
+//
+//        recyler_view.setHasFixedSize(false)
+//    }
+
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+    private lateinit var recyler_view: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_record, container, false)
+
+        return view
+    }
+
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+        recyler_view = itemView.findViewById(R.id.recyler_view)
+        trashSortBtn = itemView.findViewById(R.id.trashSort)
+        dateSortBtn = itemView.findViewById(R.id.dateSort)
+        nextMapsBtn = itemView.findViewById(R.id.next_maps)
+        rankingButton = itemView.findViewById(R.id.ranking_button)
+
+
+        nextMapsBtn.setOnClickListener {
+            val intent = Intent(activity, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        rankingButton.setOnClickListener {
+            val intent = Intent(activity, RankActivity::class.java)
+            startActivity(intent)
+        }
+
+        work()
+    }
+
+
+    private fun setAdapter(PloggingList : List<PloggingResult>){
+        val mAdapter = RecyclerAdapter(PloggingList)
+        recyler_view.adapter = mAdapter
+        recyler_view.layoutManager = LinearLayoutManager(activity)
+
+        recyler_view.setHasFixedSize(false)
+    }
+
+}
